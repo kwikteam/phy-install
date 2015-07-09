d241c71275d12ed6358e2895bea40b9a05ffb715
Max Hunter <nippoo@nippoo.net>
2015-07-09 18:51:05 +0100

    Bugfix on Windows

--- latest.ps1
+++ latest.ps1
@@ -1,5 +1,5 @@
 pushd ~
-(new-object System.Net.WebClient).DownloadFile('https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe', 'miniconda3.exe')
+(new-object System.Net.WebClient).DownloadFile('https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe', "$Home\miniconda3.exe")
 .\miniconda3.exe /RegisterPython=1 /S /D="$Home\miniconda3"
 $env:Path += ";$Home\miniconda3\;$Home\miniconda3\Scripts"
 conda install conda python=3 pip numpy matplotlib scipy h5py pyqt ipython-notebook requests vispy -y
