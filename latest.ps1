b1711c07ad987c3351912f3346e263a5338dc546
Max Hunter <nippoo@nippoo.net>
2015-08-07 17:09:21 +0100

    Hard-code py34 rather than py3 in conda install

--- latest.ps1
+++ latest.ps1
@@ -2,7 +2,7 @@ pushd ~
 (new-object System.Net.WebClient).DownloadFile('https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe', "$Home\miniconda3.exe")
 .\miniconda3.exe /RegisterPython=1 /S /D="$Home\miniconda3"
 $env:Path += ";$Home\miniconda3\;$Home\miniconda3\Scripts"
-conda install conda python=3 pip numpy matplotlib scipy h5py pyqt ipython-notebook requests vispy six -y
+conda install conda python=3.4 pip numpy matplotlib scipy h5py pyqt ipython-notebook requests vispy six -y
 conda install -c https://conda.binstar.org/kwikteam klustakwik2 -y
 pip install vispy phy
 popd
